4e2d98bb4c99bc9441b5458c5c6578500e2fd62a
karthik <karthikhs1402@gmail.com>
2025-10-07 06:23:01 +0530

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback
+	}
+	portString := fmt.Sprintf(":%s", port)
+	fmt.Printf("Server running on :%s\n", portString)
+
+	mux := http.NewServeMux()
+
+	apiHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hit", time.Now())
+		fmt.Fprintf(w, "API endpoint - Hit at %s\n", time.Now().Format(time.RFC3339))
+	})
+
+	mux.Handle("/api", apiHandler)
+
+	http.ListenAndServe(portString, mux)
+}
